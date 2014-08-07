40bef3b1c47770a96e60cd224142515cda5bf353
1BrandonCummings <brandoncummingsfl@gmail.com>
2014-08-06 20:24:15 -0400

    added pics page with looping erb for pic urls

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'sitemap' => 'sitemap#index'
-  get 'welcome/about_me' => 'welcome#about_me'
+  get 'aboutme' => 'aboutme'
   get 'welcome' => 'welcome#index'
-
+  get 'aboutme/pics'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
