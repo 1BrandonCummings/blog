95587971ac1d12661351166239f712268736b5d6
1BrandonCummings <brandoncummingsfl@gmail.com>
2014-08-11 19:58:18 -0400

    Got the correct bootstrap and Header and Footers in place

--- app/controllers/aboutme_controller.rb
+++ app/controllers/aboutme_controller.rb
@@ -3,6 +3,24 @@ class AboutmeController < ApplicationController
 	end
 
 	def pics
+
+		description1 ="This is my girlfriend and I dancing at our friend's wedding."
+		description2 ="I am standing next a Redwood tree located in Eastpoint, Florida. I brought this Redwood tree back from California when it was 3 Years old and no bigger than my hand, it is now 5 years old and much, much bigger."
+		description3 ="I gave my Dad a GoPro for his birthday and I was testing it out in the water for him."
+		
+		image1 = "assets/dancing.jpg"
+		image2 = "assets/redwood.jpg"
+		image3 = "assets/swimming.jpg"
+
+
+		 @image_urls = {
+
+			description1 => image1,
+
+	 		description2 => image2,  
+	 
+	 		description3 => image3 
+	 } 
 	end
 	
 end
