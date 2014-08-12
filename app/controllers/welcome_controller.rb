ed350812829630085d9adbdd15532c197182e814
1BrandonCummings <brandoncummingsfl@gmail.com>
2014-08-11 20:33:44 -0400

    added sessions helper

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,17 @@
 class WelcomeController < ApplicationController
   def index
+  	#get current date and time in string
+  	#store date and time in session
+  	#pull date and time _from_ the session and make available to the view
+
+		current_dt = Time.now.strftime("%B %d, %Y, %A %H %M %S")
+
+		
+		if session["first_visit"] == nil
+			session["first_visit"] = current_dt
+		end
+		@first_visit = session["first_visit"]
+	
   end
 
   def about_me
