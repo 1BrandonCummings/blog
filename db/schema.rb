a1d01d0244d37f3b7b4d4c28cd7fbe1667c1d569
1BrandonCummings <brandoncummingsfl@gmail.com>
2014-08-24 17:47:40 -0400

    added new migration and got models working locally

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823123428) do
+ActiveRecord::Schema.define(version: 20140824135456) do
 
   create_table "pictures", force: true do |t|
     t.string   "name"
     t.string   "location"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "projects", force: true do |t|
