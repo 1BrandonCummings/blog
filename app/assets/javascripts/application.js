4870ad35bfae15ea75f982ce585a07eaeda2a5b6
1BrandonCummings <brandoncummingsfl@gmail.com>
2014-08-24 19:07:01 -0400

    sweet parrallaxing break throughs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,11 +19,11 @@ $(document).ready(function(){
 var $window = $(window); 
 
 $('section[data-type="background"]').each(function(){
-var $bgobj = $(this); // assigning the object
+	var $bgobj = $(this); // assigning the object
 $(window).scroll(function() {
-var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+	var yPos = -($window.scrollTop() / $bgobj.data('speed'));
 // Put together our final background position
-var coords = '50% '+ yPos + 'px';
+	var coords = '50% '+ yPos + 'px';
 // Move the background
 $bgobj.css({ backgroundPosition: coords });
 });
